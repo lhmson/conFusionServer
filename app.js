a4f01f6a92ec96ed590f3da8bf8b2b19ca421163
lhmson <lehoangminhson123@gmail.com>
2020-10-04 18:53:44 +0700

    Basic authentication

--- app.js
+++ app.js
@@ -20,6 +20,36 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// handle basic authentication
+function auth(req, res, next) {
+  console.log(req.headers);
+  var authHeader = req.headers.authorization;
+  if (!authHeader) {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+    return;
+  }
+
+  var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+    .toString()
+    .split(":");
+  var user = auth[0];
+  var pass = auth[1];
+  if (user == "admin" && pass == "password") {
+    next(); // authorized
+  } else {
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate", "Basic");
+    err.status = 401;
+    next(err);
+  }
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
@@ -29,18 +59,24 @@ app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 
 // connect to mongodb server
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Dishes = require('./models/dishes');
-const Promotions = require('./models/dishes');
-const Leaders = require('./models/dishes');
+const Dishes = require("./models/dishes");
+const Promotions = require("./models/dishes");
+const Leaders = require("./models/dishes");
 
-const url = 'mongodb://localhost:27017/conFusion';
+// run server on mongodb folder: mongod --dbpath=data --bind_ip 127.0.0.1
+const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
+connect.then(
+  (db) => {
     console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
