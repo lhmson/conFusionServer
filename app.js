d7180d033ba7d666acc0eddec2de13ed44d6e099
lhmson <lehoangminhson123@gmail.com>
2020-10-04 18:53:14 +0700

    Assignment 2

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ app.use("/leaders", leaderRouter);
 const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
+const Promotions = require('./models/dishes');
+const Leaders = require('./models/dishes');
 
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url);
