32f279fbc0d91cc1dae0c21d5576f5e06ce552cd
lhmson <lehoangminhson123@gmail.com>
2021-04-05 20:35:48 +0700

    Upload files

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var usersRouter = require("./routes/users");
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promoRouter");
 var leaderRouter = require("./routes/leaderRouter");
+const uploadRouter = require("./routes/uploadRouter");
 
 // connect to mongodb server
 const mongoose = require("mongoose");
@@ -95,6 +96,7 @@ app.use("/users", usersRouter);
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
