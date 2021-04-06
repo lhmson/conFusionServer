893bae80497458a14ff9021d38e82382ec8f5e88
lhmson <lehoangminhson123@gmail.com>
2021-04-06 16:03:34 +0700

    Integrate OAuth with Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "301620851609134",
+    clientSecret: "6d6d338696ecb914a141ed1aab49f02a",
+  },
 };
